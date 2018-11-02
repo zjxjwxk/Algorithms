1efcb4e705ec8398815569fa8ff870ccf21051c0
zjxjwxk <zjxjwxk@gmail.com>
2018-11-02 12:33:55 +0800

    BTH004 Exercise2

--- src/BTH004Exercise2.java
+++ src/BTH004Exercise2.java
@@ -1,7 +1,5 @@
 public class BTH004Exercise2 {
 
-    public static final int A = 2;
-
     public static int[] findMaxAndMin(int[] a) {
         int len = a.length;
         int min, max;
@@ -54,11 +52,81 @@ public class BTH004Exercise2 {
         return new int[]{max, min};
     }
 
+    public static int findSecondMax(int[] a){
+        int[] maxTwo = getNextArr(a);
+        if (maxTwo[0] > maxTwo[1]) {
+            return maxTwo[1];
+        } else {
+            return maxTwo[0];
+        }
+    }
+
+    private static int[] getNextArr(int[] a) {
+        int len = a.length;
+        if (len == 2) {
+            return a;
+        }
+        int nextArrLen = len / 2;
+        int j = 0;
+        int[] nextArr = new int[nextArrLen];
+        for (int i = 0; i < len; i += 2) {
+            if (a[i] > a[i + 1]) {
+                nextArr[j++] = a[i];
+            } else {
+                nextArr[j++] = a[i + 1];
+            }
+        }
+        printArr(nextArr);
+        return getNextArr(nextArr);
+    }
+
+    public static void printArr(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static int findMedian(int[] a) {
+        if (a[0] < a[1]) {
+            if (a[2] < a[0]) {
+                return a[0];
+            }
+            if (a[2] > a[1]) {
+                return a[1];
+            }
+            return a[2];
+        } else {
+            if (a[2] > a[0]) {
+                return a[0];
+            }
+            if (a[2] < a[1]) {
+                return a[1];
+            }
+            return a[2];
+        }
+    }
+
     public static void main(String[] args) {
 
-        int a[] = {4, 2, 7, 0, 3, 8, 11, 1};
-        int result[] = findMaxAndMin(a);
-        System.out.println("max:" + result[0]);
-        System.out.println("min:" + result[1]);
+        /*
+          findMaxAndMin test
+         */
+//        int a[] = {4, 2, 7, 0, 3, 8, 11, 1};
+//        int result[] = findMaxAndMin(a);
+//        System.out.println("max:" + result[0]);
+//        System.out.println("min:" + result[1]);
+
+        /*
+          findSecondMax test
+         */
+//        int[] a = {4, 8, 1, 3, 6, 10, 7, 9, 13, 2, 15, 5, 16, 11, 14, 16};
+//        System.out.println("Second max:" + findSecondMax(a));
+
+        /*
+          findMedian test
+         */
+        int[] a = {2, 1, 3};
+        System.out.println("Median:" + findMedian(a));
     }
 }
