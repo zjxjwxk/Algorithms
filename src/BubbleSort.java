798ceeee73497b58bfbbe6fd46cf1462f5c82efc
zjxjwxk <zjxjwxk@gmail.com>
2018-11-02 12:33:23 +0800

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,30 @@
+/**
+ * @author zjxjwxk
+ */
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j < arr.length - i - 1; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    private static void print(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, 4, 9, 1, 5, 7, 6, 8};
+        bubbleSort(arr);
+        print(arr);
+    }
+}
