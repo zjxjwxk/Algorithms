96c0a34216dccd41b65f151624edc8a9c4b63759
zjxjwxk <zjxjwxk@gmail.com>
2018-11-30 08:47:35 +0800

    CountingSort

--- src/bth004/homework/CountingSort.java
+++ src/bth004/homework/CountingSort.java
@@ -1,42 +1,52 @@
 package bth004.homework;
 
 /**
- * 0~99的计数排序
+ * Counting sort of 0~99
  * @author zjxjwxk
  */
 public class CountingSort {
 
+    /**
+     * Counting sort of 0~99
+     * @param arr array to sort
+     * @return sorted array
+     */
     public static int[] countingSort(int[] arr) {
 
         int len = arr.length;
 
         int[] result = new int[len];
 
-        // 假设arr中均为0~99的数，k为所需的用于计数的数组长度
+        // k is the length of the count array
         int k = 100;
 
         int[] count = new int[k];
 
-        // 计数
+        // Counting
         for (int i = 0; i < len; i++) {
-            count[arr[i]]++;
+            int a = arr[i];
+            count[a]++;
         }
 
-        // 累加计数
+        // Accumulate count
         for (int i = 1; i < k; i++) {
             count[i] = count[i] + count[i - 1];
         }
 
-        // 反向填充结果数组
-        for (int i = len - 1; i >= 0; i--) {
+        // Reverse fill result array
+        for (int i = 0; i < len; i ++) {
             int a = arr[i];
-            result[count[a] - 1] = arr[i];
+            result[count[a] - 1] = a;
             count[a]--;
         }
 
         return result;
     }
 
+    /**
+     * print array
+     * @param arr array to print
+     */
     public static void printArr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -45,8 +55,10 @@ public class CountingSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 2, 1, 9, 99, 0};
+        int[] arr = {4, 5, 2, 1, 9, 99, 0, 9, 4, 5};
+        System.out.println("Before counting sort:");
         printArr(arr);
+        System.out.println("After counting sort:");
         int[] result = countingSort(arr);
         printArr(result);
     }
